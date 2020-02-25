71d6d580aaf9112d6424d14b9664639996a5df0e
kevlol33 <eugkevin@gmail.com>
2020-02-25 16:47:06 +0100

    mise au propre

--- api/controllers/sujetforumcontrolleur.js
+++
@@ -1,7 +0,0 @@
-//***** Contoleur de la page acceuil *****//
-module.exports = {
-    //*** Permet de rester sur la pasge home ***//
-    get: (req, res) => {
-        res.render('sujetforumcontrolleurdescription')
-    },
-}
